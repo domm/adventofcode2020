367b095998a57b0344bcd492f8097f987aa729b2
Thomas Klausner <domm@plix.at>
2020-12-05 16:00:59 +0100

    more golf

--- 05_1_golf.pl
+++ 05_1_golf.pl
@@ -4,5 +4,5 @@ use 5.030;
 
 # $a=0;while(<STDIN>){tr/FBLR/0101/;$b=eval'0b'.substr($_,0,7).'*8+0b'.substr($_,7,3);if($b>$a){$a=$b}}say $a;
 
-while(<STDIN>){tr/FBLR/0101/;s/(.{7})(.{3})/'0b'.$1.'*8+0b'.$2/ee;if($_>$a){$a=$_}}say$a;
+while(<>){tr/FBLR/0101/;s/(.{7})(.{3})/'0b'.$1.'*8+0b'.$2/ee;if($_>$a){$a=$_}}say$a;
 
