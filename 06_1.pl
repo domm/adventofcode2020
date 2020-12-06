e46fd3b106f77bb0736696b49b741b122f0b02fb
Thomas Klausner <domm@plix.at>
2020-12-06 10:24:18 +0100

    cleanup 06-1, fix an irrelvant bug

--- 06_1.pl
+++ 06_1.pl
@@ -5,18 +5,15 @@ use 5.030;
 my $count;
 my %gq;
 while (<>) {
-    chomp();
-    my @q = split //;
-    if (@q == 0) {
-        use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \%gq;
-        say scalar keys %gq;
-        $count += scalar keys %gq;
+    chomp;
+    if (!$_) {
+        $count += keys %gq;
         %gq=();
     }
-    for (@q) {
-        $gq{$_}++;
+    else {
+        map { $gq{$_}++ } split //;
     }
 }
-$count += scalar keys %gq;
+$count += keys %gq;
 
 say $count;
