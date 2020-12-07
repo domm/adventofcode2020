a86cdf1462f89bb7548f5f16bcf51f81149ec499
Thomas Klausner <domm@plix.at>
2020-12-07 22:08:05 +0100

    more golf

--- 07_2_golf.pl
+++ 07_2_golf.pl
@@ -2,4 +2,4 @@ use strict;
 use warnings;
 use 5.030;
 
-my%a;for(<>){my($a,$b)=$_=~/^(.*?).bags.contain.(.*)\.$/; $a{$a}=[grep{$_->[1]=~/^\d+$/}map{/^(\d+).(.*?).bags?/;[$2,$1]}split(/,./,$b)]}my$c=sub{my($a,$b)=($_[0],1);map{$b+=$_->[1]*__SUB__->($_->[0])}$a{$a}->@*;$b};say $c->(qq{shiny$"gold})-1;
+my%a;for(<>){($a,$b)=$_=~/^(.*?).ba.*ain.(.*)\.$/;$a{$a}=[grep{$_->[1]=~/\d+/}map{/(\d+).(.*?).bags?/;[$2,$1]}split/,./,$b]}$a=sub{my($a,$b)=($_[0],1);map{$b+=$_->[1]*__SUB__->($_->[0])}$a{$a}->@*;$b};say&$a("shiny$\"gold")-1
