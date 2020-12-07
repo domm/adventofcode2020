cf76d0c902379751ae5638902e5e69ca2f98544f
Thomas Klausner <domm@plix.at>
2020-12-07 17:02:25 +0100

    cleaned up day 07

--- 07_1.pl
+++ 07_1.pl
@@ -2,40 +2,22 @@ use strict;
 use warnings;
 use 5.030;
 
-#posh brown bags contain 5 dim coral bags, 1 plaid blue bag, 2 faded bronze bags, 2 light black bags.
-#vibrant lime bags contain 3 dull lavender bags, 3 dim crimson bags, 3 mirrored lavender bags, 2 muted cyan bags.
-
-
-my %bags;
 my %contained;
-while (<>) {
+for (<>) {
     my ($container, $content) = $_ =~ /^(.*?) bags contain (.*)\.$/;
-    my @content = map { say $_;/^\d+ (.*?) bags?/} split(/, /,$content);
-    #say $container ." -> ".join (', ',@content);
-
-    $bags{$container} = \@content;
+    my @content = map {/^\d+ (.*?) bags?/} split(/, /,$content);
     foreach (@content) {
         push($contained{$_}->@*,$container);
     }
 }
 
-my %found;
-
-count_bag('shiny gold');
+say count_bag('shiny gold', {});
 
 sub count_bag {
-    my $bag=shift;
-    my $parents = $contained{$bag};
-    foreach my $p ($parents->@*) {
-        $found{$p}++;
-        count_bag($p);
+    my ($bag, $agg) = @_;
+    foreach my $p ($contained{$bag}->@*) {
+        $agg->{$p}++;
+        count_bag($p, $agg);
     }
+    scalar keys $agg->%*;
 }
-
-use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \%contained;
-
-use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \%found;
-
-say scalar keys %found;
-
-
--- 07_2.pl
+++ 07_2.pl
@@ -3,35 +3,21 @@ use warnings;
 use 5.030;
 
 my %bags;
-my %contained;
-while (<>) {
+for (<>) {
     my ($container, $content) = $_ =~ /^(.*?) bags contain (.*)\.$/;
-    my @content = map { /^(\d+) (.*?) bags?/; [ $2,$1 ] } split(/, /,$content);
+    my @content = map {/^(\d+) (.*?) bags?/; [ $2,$1 ] } split(/, /,$content);
     next if $content[0][0] eq 'no other bags';
 
     $bags{$container} = \@content;
-    foreach (@content) {
-        push($contained{$_}->@*,$container);
-    }
 }
 
-my %found;
-my $count = count_bag('shiny gold');
+say count_bag('shiny gold') - 1;
 
 sub count_bag {
-    my ($bag) = @_;
-    say "count $bag";
-    my $children = $bags{$bag};
-    my $thiscount = 1;
-    foreach my $c ($children->@*) {
-        say "$bag contains ".join(', ',@$c);
-        my $factor = $c->[1];
-        my $cc = count_bag($c->[0]);
-        say "$factor * $cc";
-        $thiscount += $factor * $cc;
+    my $bag = shift;
+    my $count = 1;
+    foreach my $c ($bags{$bag}->@*) {
+        $count += $c->[1] * count_bag($c->[0]);
     }
-    return $thiscount;
+    return $count;
 }
-
-say $count - 1;
-
