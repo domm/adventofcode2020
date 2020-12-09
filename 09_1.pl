04575bf108fbe3575dff757a21361ab186194521
Thomas Klausner <domm@plix.at>
2020-12-09 20:44:08 +0100

    cleanup

--- 09_1.pl
+++ 09_1.pl
@@ -2,32 +2,21 @@ use strict;
 use warnings;
 use 5.030;
 
-my $preamble=shift;
-my @in=map {chomp;$_} <>;
-#use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \@in;
+my $size = shift;
+my @in   = map { chomp; $_ } <>;
 
-my $cnt=0;
 while (1) {
-    say "rount ".$cnt++;
-    if (check(@in)) {
-        shift @in;
-    }
-    else {
-        say "found ".$in[$preamble ];
-        exit;
-    }
+    check() ? shift @in : last;
 }
+say $in[$size];
 
 sub check {
-    my @data = @_;
-    my %pre = map {$_,1} splice(@data, 0, $preamble);
-
-    my $target = $data[0];
-    for my $a (keys %pre) {
-        if ($pre{$target - $a}) {
+    my %lu = map { $_,1 } @in[0 .. $size-1];
+    my $target = $in[$size];
+    for my $a ( keys %lu ) {
+        if ( $lu{ $target - $a } ) {
             return 1;
         }
     }
-    return 0
+    return;
 }
-
