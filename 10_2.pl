fa5c50b979c7784c2a461d6ee54c63630bb75956
Thomas Klausner <domm@plix.at>
2020-12-10 17:53:20 +0100

    cleanup

--- 10_2.pl
+++ 10_2.pl
@@ -2,20 +2,19 @@ use strict;
 use warnings;
 use 5.030;
 
-my @adapters = sort { $a <=> $b } map { chomp; $_ } <>;
-unshift(@adapters,0);
-push(@adapters,$adapters[-1]+3);
-my @lazy_caterer=(undef,1,1,2,4,7);
-my @set=(0);
-my $cnt = 1;
-for (my $i=0;$i < scalar @adapters;$i++) {
-    my $a = $adapters[$i];
-    if ($adapters[$i + 1] && ($adapters[$i + 1] - $a) == 3) {
-        $cnt *= $lazy_caterer[@set];
-        @set=($a);
-    }
-    else {
-        push (@set, $a);
+# https://old.reddit.com/r/adventofcode/comments/kacv5i/2020_day_10_p2_need_some_logic_help_not_specific/gf9nsaf/?context=3
+
+my @lazy_caterer = ( undef, 1, 1, 2, 4, 7 );
+my @adapters     = sort { $a <=> $b } 0, map { chomp; $_ } <>;
+push( @adapters, $adapters[-1] + 3 );
+my @set = 0;
+my $res = 1;
+my $i   = 1;
+for (@adapters) {
+    push( @set, $_ );
+    if ( ( $adapters[ $i++ ] - $set[-1] ) == 3 ) {
+        $res *= $lazy_caterer[ @set - 1 ];
+        @set = $set[-1];
     }
 }
-say $cnt;
+say $res;
