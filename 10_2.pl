2174c0e8b8100c97cabfceb0918c34d9e231824a
Thomas Klausner <domm@plix.at>
2020-12-10 17:40:53 +0100

    lazy caterer

--- 10_2.pl
+++ 10_2.pl
@@ -5,31 +5,17 @@ use 5.030;
 my @adapters = sort { $a <=> $b } map { chomp; $_ } <>;
 unshift(@adapters,0);
 push(@adapters,$adapters[-1]+3);
-say $adapters[0];
-say $adapters[-1];
-my %factors = (
-    1=>1,
-    2=>1,
-    3=>2,
-    4=>4,
-    5=>7
-);
+my @lazy_caterer=(undef,1,1,2,4,7);
 my @set=(0);
 my $cnt = 1;
 for (my $i=0;$i < scalar @adapters;$i++) {
     my $a = $adapters[$i];
-    say $a;
-    if (($adapters[$i + 1] - $a) == 3) {
-        my $size = @set;
-        die unless $factors{$size};
-        my $factor = $factors{$size};
-        $cnt *= $factors{$size};
-        say "new set $size factor $factor -> $cnt";
+    if ($adapters[$i + 1] && ($adapters[$i + 1] - $a) == 3) {
+        $cnt *= $lazy_caterer[@set];
         @set=($a);
     }
     else {
         push (@set, $a);
     }
 }
-#$cnt *= $factors{scalar @set};
 say $cnt;
