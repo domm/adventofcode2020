58d267f972f3f1c39bb60c7883265b4bc673372f
Thomas Klausner <domm@plix.at>
2020-12-11 17:05:22 +0100

    cleanup

--- 11_1.pl
+++ 11_1.pl
@@ -1,48 +1,49 @@
 use strict;
-#use warnings;
+use warnings;
 use 5.030;
 
-$|=1;
-my @map = map {chomp; [split//] }  <>;
-my $h = @map;
-my $w = $map[0]->@*;
-my $occ=0;
-my @around=([-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]);
+my @map = map { chomp; [ split // ] } <>;
+my $h   = @map;
+my $w   = $map[0]->@*;
+my $occ = 0;
+my $max = 4;
+my @around =
+    ( [ -1, -1 ], [ -1, 0 ], [ -1, 1 ], [ 0, -1 ], [ 0, 1 ], [ 1, -1 ], [ 1, 0 ], [ 1, 1 ] );
 
-while(1) {
+while (1) {
     my @next;
-    my $thisocc=0;
-    for (my $r=0;$r<$h;$r++) {
-        for (my $c=0;$c<$w;$c++) {
-            my $old=$map[$r][$c];
-            my $neigh=0;
-            foreach my $l (@around) {
-                my $m=$r+$l->[0];
-                my $n=$c+$l->[1];
-                next if $m<0 || $n<0;
-                my $val = $map[$m]->[$n];
-                $neigh++ if $val eq '#';
-               }
-            if ($old eq 'L' && $neigh == 0) {
-                $next[$r][$c] = '#'
-            }
-            elsif ($old eq '#' && $neigh >= 4) {
-                $next[$r][$c] = 'L'
-            }
-            else {
-                $next[$r][$c] = $old;
-            }
-            $thisocc++ if $next[$r][$c] eq '#';
-            #print $next[$r][$c];
-
+    my $thisocc = 0;
+    for ( my $r = 0; $r < $h; $r++ ) {
+        for ( my $c = 0; $c < $w; $c++ ) {
+            my $new = $next[$r][$c] = gol( \@map, $r, $c );
+            $thisocc++ if $new eq '#';
         }
-        #print"\n";
     }
-    print '.';
-    last if ($thisocc == $occ);
+    last if $thisocc == $occ;
     @map = @next;
-    $occ=$thisocc;
-    #print "\n";
+    $occ = $thisocc;
+    say '.';
 }
-say "\n".$occ;
+say $occ;
 
+sub gol {
+    my ( $map, $r, $c ) = @_;
+
+    my $count = look( $map, $r, $c );
+    my $old   = $map[$r][$c];
+    return '#' if $old eq 'L' && $count == 0;
+    return 'L' if $old eq '#' && $count >= $max;
+    return $old;
+}
+
+sub look {
+    my ( $map, $r, $c ) = @_;
+    my $count = 0;
+    foreach my $vec (@around) {
+        my $m = $r + $vec->[0];
+        my $n = $c + $vec->[1];
+        next     if $m < 0 || $n < 0 || $m >= $h || $n >= $w;
+        $count++ if $map[$m][$n] eq '#';
+    }
+    return $count;
+}
--- 11_2.pl
+++ 11_2.pl
@@ -1,63 +1,75 @@
 use strict;
-#use warnings;
+use warnings;
 use 5.030;
 
-$|=1;
-my @map = map {chomp; [split//] }  <>;
-my $h = @map;
-my $w = $map[0]->@*;
-my $occ=0;
-my @around=([-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]);
+my @map = map { chomp; [ split // ] } <>;
+my $h   = @map;
+my $w   = $map[0]->@*;
+my $occ = 0;
+my $max = 5;
+my @around =
+    ( [ -1, -1 ], [ -1, 0 ], [ -1, 1 ], [ 0, -1 ], [ 0, 1 ], [ 1, -1 ], [ 1, 0 ], [ 1, 1 ] );
 
-while(1) {
+while (1) {
     my @next;
-    my $thisocc=0;
-    for (my $r=0;$r<$h;$r++) {
-        for (my $c=0;$c<$w;$c++) {
-            my $old=$map[$r][$c];
-            my $count = look(\@map, $r, $c);
-            if ($old eq 'L' && $count == 0) {
-                $next[$r][$c] = '#'
-            }
-            elsif ($old eq '#' && $count >= 5) {
-                $next[$r][$c] = 'L'
-            }
-            else {
-                $next[$r][$c] = $old;
-            }
-            $thisocc++ if $next[$r][$c] eq '#';
-            #print $next[$r][$c];
-
+    my $thisocc = 0;
+    for ( my $r = 0; $r < $h; $r++ ) {
+        for ( my $c = 0; $c < $w; $c++ ) {
+            my $new = $next[$r][$c] = gol( \@map, $r, $c );
+            $thisocc++ if $new eq '#';
         }
-        #print"\n";
     }
-    print '.';
-    last if ($thisocc == $occ);
+    last if $thisocc == $occ;
     @map = @next;
-    $occ=$thisocc;
-    #print "\n";
+    $occ = $thisocc;
+    say '.';
 }
-say "\n".$occ;
+say $occ;
 
+sub gol {
+    my ( $map, $r, $c ) = @_;
+
+    my $count = look( $map, $r, $c );
+    my $old   = $map[$r][$c];
+    return '#' if $old eq 'L' && $count == 0;
+    return 'L' if $old eq '#' && $count >= $max;
+    return $old;
+}
 sub look {
-    my ($map, $r, $c) = @_;
-    my $count=0;
+    my ( $map, $r, $c ) = @_;
+    my $count = 0;
     foreach my $vec (@around) {
-
-        my $tr=$r;
-        my $tc=$c;
+        my $tr = $r;
+        my $tc = $c;
         while (1) {
-            my $m=$tr+$vec->[0];
-            my $n=$tc+$vec->[1];
-            last if $m<0 || $n<0 || $m > $h || $n > $w;
-            my $val = $map->[$m][$n];
+            my $m = $tr + $vec->[0];
+            my $n = $tc + $vec->[1];
+            last if $m < 0 || $n < 0 || $m >= $h || $n >= $w;
+            my $val = $map[$m][$n];
             $count++ if $val eq '#';
-            last unless $val  eq '.';
+            last unless $val eq '.';
             $tr = $m;
             $tc = $n;
         }
     }
     return $count;
 }
-
-
+sub look2 {
+    my ( $map, $r, $c ) = @_;
+    my $count = 0;
+    foreach my $vec (@around) {
+        my $tr = $r;
+        my $tc = $c;
+        while (1) {
+            my $m = $tr + $vec->[0];
+            my $n = $tc + $vec->[1];
+            last if $m < 0 || $n < 0 || $m >= $h || $n >= $w;
+            my $val = $map[$m][$n];
+            $count++ if $val eq '#';
+            last unless $val eq '.';
+            $tr = $m;
+            $tc = $n;
+        }
+    }
+    return $count;
+}
