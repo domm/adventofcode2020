ff97861ca76cde7c745cd69023faab47edc5be31
Thomas Klausner <domm@plix.at>
2020-12-12 15:10:41 +0100

    works

--- 12_1.pl
+++ 12_1.pl
@@ -4,51 +4,32 @@ use 5.030;
 no strict 'refs';
 
 my @in = map { [/^(\w)(\d+)$/] }  <>;
-
-my $dir = 'E';
-my @map;
-
-my %turn=(
-    L => [qw(N W S E)],
-    R => [qw(S W N E)]
-);
-
-my $r=1000;
-my $c=1000;
+my @turn=qw(N E S W);
+my $dir = 1;
+my $r=0;
+my $c=0;
 
 for (@in) {
     my ($op, $val) = @$_;
     &$op($val);
-    say "$r  / $c";
 }
-
-say abs($r-1000) + abs($c-1000);
-
+say abs(0-$r) + abs(0-$c);
 
 sub N { $r -= shift }
 sub S { $r += shift }
 sub E { $c += shift }
 sub W { $c -= shift }
-
 sub F {
     my $val = shift;
-    $dir->($val);
+    $turn[$dir]->($val);
 }
-
 sub L {
     my $val = shift;
-    turn('L',$val);
+    my $times = $val / 90;
+    $dir = ($dir - $times) % 4;
 }
 sub R {
     my $val = shift;
-    turn('R',$val);
-}
-
-sub turn {
-    my ($to, $val) = @_;
     my $times = $val / 90;
-    for (1 .. $times) {
-        my $new = $dir = shift($turn{$to}->@*);
-        push($turn{$to}->@*, $new);
-    }
+    $dir = ($dir + $times) % 4;
 }
