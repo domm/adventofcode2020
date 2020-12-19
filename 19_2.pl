77acff17c5439994b5be6454e5af26eb5f6d99c2
Thomas Klausner <domm@plix.at>
2020-12-19 18:14:10 +0100

    find correct number of tries we need to get "static" results

--- 19_2.pl
+++ 19_2.pl
@@ -13,18 +13,32 @@ for (split(/\n/,$in[0])) {
 $rules{8} = ' 42 | 42 8 ';
 $rules{11} = ' 42 31 | 42 11 31';
 
-my $raw = $rules{0};
-my $cnt=0;
-while ($raw =~ /\d/) {
-    $raw=~s/(\d+)/(?:$rules{$1})/g;
-    last if $cnt++ > 12;
-}
-$raw=~s/ //g;
-my $regex=qr/^$raw$/;
+my $try=1;
+my $hits=-1;
+while (1) {
+    say "TRY $try";
+    my $cnt = 0;
+    my $raw = $rules{0};
+    while ($raw =~ /\d/) {
+        $raw=~s/(\d+)/(?:$rules{$1})/g;
+        last if $cnt++ > $try;
+    }
+    $raw=~s/ //g;
+    my $regex=qr/^$raw$/;
 
-my $hit=0;
-for my $l (split(/\n/,$in[1])) {
-    $hit++ if $l=~/$regex/;
+    my $hit=0;
+    for my $l (split(/\n/,$in[1])) {
+        $hit++ if $l=~/$regex/;
+    }
+    say "got $hit";
+    if ($hit) {
+        if ($hits == $hit) {
+            last;
+        }
+        $hits = $hit;
+    }
+    $try++;
 }
-say $hit;
+
+say "final $hits";
 
