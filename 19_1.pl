b4fa761e4566af2f6c87f685d742c8ef4be1b69c
Thomas Klausner <domm@plix.at>
2020-12-19 11:16:54 +0100

    clean

--- 19_1.pl
+++ 19_1.pl
@@ -7,9 +7,7 @@ use Parse::RecDescent;
 my @in=split(/\n\n/,join('',<>));
 my $grammar = $in[0];
 $grammar=~s/(\d+)/r$1/g;
-say $grammar;
 my $parser = new Parse::RecDescent("startrule: r0 ';'\n".$grammar);
-say $parser;
 
 my $hit=0;
 my @data=split(/\n/,$in[1]);
@@ -17,7 +15,6 @@ for my $l (@data) {
     $l.=';';
     if ($parser->startrule($l)) {
         $hit++;
-        say "$l";
     }
 }
 say $hit;
