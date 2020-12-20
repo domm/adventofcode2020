9a17db3738f0ec0db1f3e703a1da1283da519d26
Thomas Klausner <domm@plix.at>
2020-12-20 13:41:21 +0100

    uff, 20 1 done

--- 20_1.pl
+++ 20_1.pl
@@ -8,7 +8,6 @@ for(split(/\n\n/,join('',<>))) {
     my @raw = split(/\n/);
     my $name = shift(@raw);
     my ($id)= $name =~ /(\d+)/;
-    say $id;
 
     my @tile;
     for(@raw) {
@@ -16,33 +15,22 @@ for(split(/\n\n/,join('',<>))) {
         push(@tile,[split//]);
     }
 
-    my $top = checksum($tile[0]);
-    my $bottom = checksum($tile[9]);
-    my $left = checksum([ map { $tile[$_][0] } 0..9 ]);
-    my $right = checksum([ map { $tile[$_][9] } 0..9 ]);
-
-    $matches{$top}++;
-    $matches{$bottom}++;
-    $matches{$left}++;
-    $matches{$right}++;
-
-    $tiles{$id}=[$top, $bottom, $left, $right];
+    my @sides;
+    foreach ($tile[0], $tile[9], [ map { $tile[$_][0] } 0..9 ], [ map { $tile[$_][9] } 0..9 ]) {
+        my $side = join ('',$_->@*);
+        my $flip = join ('',reverse $_->@*);
+        my @sorted = sort ($side, $flip);
+        my $choose = $sorted[0];
+        push(@sides,$choose);
+        $matches{$choose}++;
+    }
+    $tiles{$id} = \@sides;
 }
 
 my %edges = map { $_=>1} grep { $matches{$_} == 1 } keys %matches;
-
-use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \%edges;
-
-my @corners;
+my $prod=1;
 while (my ($id,$borders) = each %tiles) {
-    say $id;
     my @foo = grep { $edges{$_}} @$borders;
-    say scalar @foo;
+    $prod *= $id if (@foo == 2);
 }
-
-sub checksum {
-    my $border = shift;
-    my $row = join('',map { /#/ ? 1 : 0 } $border->@*);
-    return eval '0b'.$row;
-}
-
+say $prod;
