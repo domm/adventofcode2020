ce203699b4f5caf7e86ccbf3b11f74943e9220c6
Thomas Klausner <domm@plix.at>
2020-12-23 10:32:00 +0100

    part 1 less loops

--- 23_1.pl
+++ 23_1.pl
@@ -2,8 +2,11 @@ use 5.020;
 use strict;
 use warnings;
 
-my @cups = map { {cup=>$_,next=>undef,prev=>undef} } split(//,$ARGV[0]);
+
+my %by_val;
+my @cups = map { my $c= {cup=>$_,next=>undef,prev=>undef};$by_val{$c->{cup}} = $c; $c } split(//,$ARGV[0]);
 my $size= @cups;
+my $max = $size - 1;
 
 for (my $i=0;$i<@cups;$i++) {
     $cups[$i]->{prev} = $cups[$i-1];
@@ -13,20 +16,23 @@ for (my $i=0;$i<@cups;$i++) {
 my $current = $cups[0];
 for my $m (1..100) {
     say "\nround $m";
-    dumpcups($current);
+
     my $remove_start = $current->{next};
-    my $remove_end = rotate($current, 3);
+    my $remove_end = $remove_start->{next}{next};
     $current->{next} = $remove_end->{next};
     $current->{next}{prev} = $current;
+    my %skip=(
+        $remove_start->{cup}=>1,
+        $remove_start->{next}{cup}=>1,
+        $remove_start->{next}{next}{cup}=>1,
+    );
 
-    my $val = ($current->{cup} - 1) || 9;
+    my $val = ($current->{cup} - 1) || $size;
     my $destination;
     while (!$destination) {
-        say "try $val";
-        $destination = find($current, $val);
-        $val = (($val-1) % $size) || 9;
+        $destination = $by_val{$val} unless $skip{$val};
+        $val = (($val-1) % $size) || $size;
     }
-    say "dest: ".$destination->{cup};
     my $destination_after = $destination->{next};
 
     $destination->{next} = $remove_start;
@@ -35,19 +41,9 @@ for my $m (1..100) {
     $remove_end->{next} = $destination_after;
 
     $current = $current->{next};
-    dumpcups($current);
 }
 
-my $start;
-for (1..9) {
-    if ($current->{cup} == 1) {
-        $start=$current;
-        last;
-    }
-    $current=$current->{next};
-}
-
-say result($start);
+say result($by_val{1});
 
 sub result {
     my $dh = shift;
@@ -59,7 +55,6 @@ sub result {
     return $res;
 }
 
-
 sub dumpcups {
     my $dh = shift;
     for (1..9) {
@@ -68,32 +63,3 @@ sub dumpcups {
     }
     print"\n";
 }
-
-sub rotate {
-    my ($h, $count) = @_;
-    my $dir = 'next';
-    for (1 .. abs($count)) {
-        $h = $h->{$dir};
-    }
-    return $h;
-}
-
-sub find {
-    my ($h, $target) = @_;
-
-    for (1..6) {
-        return $h if $h->{cup} == $target;
-        $h = $h->{next}
-    }
-    return;
-}
-
-#
-#use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \@cups;
-#
-#my $head = {
-#    marble=>0,
-#};
-#$head->{prev} = $head->{next} = $head;
-
-
