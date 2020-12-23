e069d75b582882e11192add95004209b4433fcf5
Thomas Klausner <domm@plix.at>
2020-12-23 08:29:34 +0100

    23 1

--- 23_1.pl
+++ 23_1.pl
@@ -5,25 +5,28 @@ use warnings;
 my @cups = map { {cup=>$_,next=>undef,prev=>undef} } split(//,$ARGV[0]);
 my $size= @cups;
 
-my %by_label;
 for (my $i=0;$i<@cups;$i++) {
     $cups[$i]->{prev} = $cups[$i-1];
     $cups[$i]->{next} = $cups[($i+1)% $size];
-    $by_label{$cups[$i]->{cup}} = $cups[$i];
 }
 
 my $current = $cups[0];
-for my $m (1..3) {
-    say "round $m";
+for my $m (1..100) {
+    say "\nround $m";
     dumpcups($current);
     my $remove_start = $current->{next};
     my $remove_end = rotate($current, 3);
     $current->{next} = $remove_end->{next};
     $current->{next}{prev} = $current;
 
-    my $val = $current->{cup} - 1;
-    say "des $val";
-    my $destination = $by_label{$val};
+    my $val = ($current->{cup} - 1) || 9;
+    my $destination;
+    while (!$destination) {
+        say "try $val";
+        $destination = find($current, $val);
+        $val = (($val-1) % $size) || 9;
+    }
+    say "dest: ".$destination->{cup};
     my $destination_after = $destination->{next};
 
     $destination->{next} = $remove_start;
@@ -32,7 +35,30 @@ for my $m (1..3) {
     $remove_end->{next} = $destination_after;
 
     $current = $current->{next};
+    dumpcups($current);
 }
+
+my $start;
+for (1..9) {
+    if ($current->{cup} == 1) {
+        $start=$current;
+        last;
+    }
+    $current=$current->{next};
+}
+
+say result($start);
+
+sub result {
+    my $dh = shift;
+    my $res;
+    for (1..8) {
+        $dh = $dh->{next};
+        $res.=$dh->{cup};
+    }
+    return $res;
+}
+
 
 sub dumpcups {
     my $dh = shift;
@@ -52,7 +78,15 @@ sub rotate {
     return $h;
 }
 
+sub find {
+    my ($h, $target) = @_;
 
+    for (1..6) {
+        return $h if $h->{cup} == $target;
+        $h = $h->{next}
+    }
+    return;
+}
 
 #
 #use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \@cups;
